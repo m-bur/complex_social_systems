58f1d120fa71accef72219267105acefb4c530e3
Sun  Linghang <104532551+sunlinghang@users.noreply.github.com>
2024-11-04 20:02:40 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import numpy as np
+from utils import build_first_connection
 
 
-L = 100         # side length
-L_G = 20        # neighbor side length
+L = 50       # side length
+L_G = 10        # neighbor side length
 a = L_G         # parameter for probability function
 b = a / 4       # parameter for probability function
 N = L ** 2      # total number of nodes
+
+
+df_first_con = build_first_connection(L_G, L)
+
 
 
