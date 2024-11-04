3d8437973615a16372fa2404037c97dfb9c4f7c7
Sun  Linghang <104532551+sunlinghang@users.noreply.github.com>
2024-11-04 18:23:07 +0100

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+L = 100         # side length
+L_G = 20        # neighbor side length
+a = L_G         # parameter for probability function
+b = a / 4       # parameter for probability function
+N = L ** 2      # total number of nodes
+
+
