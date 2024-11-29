d69ed2a94ce429f8a1fc0e2d47d3d5d183504d26
moritzmoritzmoritzmoritz <103280579+moritzmoritzmoritzmoritz@users.noreply.github.com>
2024-11-29 16:38:46 +0100

    experiment1 is ready

--- experiment_1.py
+++ experiment_1.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import subprocess
+import numpy as np
 
 def run_main_with_params(params):
     """
@@ -30,28 +31,33 @@ def run_in_parallel(params_list, max_cores):
     :param max_cores: Maximum number of cores to use for parallel processing
     """
     # Use the minimum of max_cores and available CPU cores
-    num_processes = min(max_cores, multiprocessing.cpu_count())
+    num_processes = min(max_cores, multiprocessing.cpu_count()) - 2
     
     # Create a pool of processes
     with multiprocessing.Pool(processes=num_processes) as pool:
         pool.map(run_main_with_params, params_list)
 
+
+def generate_parameters():
+    folder_name = "Run_1"
+    parameters = []
+    number_of_media = np.arange(1,25,3)
+    media_shift = 0.8 / number_of_media
+    target_opinion = np.linspace(-1, 0.9, num=12)
+    average_factor = 8
+    for m_shift, n_media in zip(media_shift, number_of_media):
+        for t_opinion in target_opinion:
+            for i in range(average_factor):
+                name = folder_name + f"_n_{n_media}" + f"_o_{round(t_opinion,3)}".replace(".","p")
+                parameters.append(["-- number_of_manipulated_media", str(n_media), "--manipulation_shift", str(m_shift), "--target_media_opinion", str(t_opinion), "--parent_folder", name])
+    
+    return parameters
 # Example usage
 if __name__ == "__main__":
     # List of parameter sets to pass to main.py
-    parameters = [
-        ["--parent_folder", "Folder_A", "--number_years", "0.1"],
-        ["--parent_folder", "Folder_B", "--number_years", "0.2"],
-        ["--parent_folder", "Folder_C", "--number_years", "0.1"],
-        ["--parent_folder", "Folder_D", "--number_years", "0.2"],
-        ["--parent_folder", "Folder_A", "--number_years", "0.1"],
-        ["--parent_folder", "Folder_B", "--number_years", "0.2"],
-        ["--parent_folder", "Folder_C", "--number_years", "0.1"],
-        ["--parent_folder", "Folder_D", "--number_years", "0.2"]
-    ]
+    parameters = generate_parameters()
 
     # Define the maximum number of cores to use
-    MAX_CORES = 4
-
+    MAX_CORES = 1000
     # Run in parallel
     run_in_parallel(parameters, MAX_CORES)
